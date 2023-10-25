72c95c0bd890897676d527da6c7562616661a776
Michael <michaeldanish001@gmail.com>
2023-10-25 22:52:18 +0700

    First PR

---
+++ src/test/java/Runner/TestRunner.java
@@ -0,0 +1,14 @@
+package Runner;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features="src/test/Feature",
+        glue= "StepDefinition",
+        plugin= {"html:target/HTML_report.html"}
+)
+public class TestRunner {
+}
